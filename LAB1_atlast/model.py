0dbb952e26d3417a92f34c12bf3467e07380df20
Alexander Melnyk <inboxnumberzero@zoho.com>
2016-05-03 16:40:44 +0300

    Refactor MatchMysqlDb
    For better integration with existing and future code. Also for higher safety.

--- LAB1_atlast/model.py
+++ LAB1_atlast/model.py
@@ -2,6 +2,7 @@
 
 import sys
 import warnings
+import datetime
 import MySQLdb
 
 class ScoreError(Exception):
@@ -85,6 +86,30 @@ class MatchList:
 
 
 class MatchMysqlDb(MatchList):
+    create_table_query = ('CREATE TABLE IF NOT EXISTS matches '
+                          '(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, '
+                          'team1 VARCHAR(100) NOT NULL, '
+                          'team2 VARCHAR(100) NOT NULL, '
+                          'score1 SMALLINT NOT NULL, '
+                          'score2 SMALLINT NOT NULL, '
+                          'tournament VARCHAR(256), '
+                          '"date" DATE NOT NULL)')
+    select_query = ('SELECT * FROM matches')
+    insert_query = ('INSERT INTO matches '
+                    '(team1, team2, score1, score2, tournament, date) '
+                    'VALUES'
+                    '(%s, %s, %s, %s, %s, %s)')
+    update_query = ('UPDATE matches SET '
+                    'team1=%s, team2=%s, score1=%s, score2=%s, '
+                    'tournament=%s, date=%s '
+                    'WHERE team1 = %s AND team2 = %s '
+                    'AND score1 = %s AND score2 = %s '
+                    'AND tournament = %s AND date = %s')
+    delete_query = ('DELETE FROM matches WHERE '
+                    'team1 = %s AND team2 = %s '
+                    'AND score1 = %s AND score2 = %s '
+                    'AND tournament = %s AND date = %s')
+
     def __init__(self):
         try:
             self.conn = MySQLdb.connect('127.0.0.1', 'archlab', '', 'archlab')
@@ -95,29 +120,27 @@ class MatchMysqlDb(MatchList):
     def __del__(self):
         self.conn.close()
 
+    def _getcur(self):
+        return self.conn.cursor(MySQLdb.cursors.DictCursor)
+
     def show(self):
-        cur = self.conn.cursor(MySQLdb.cursors.DictCursor)
+        cur = self._getcur()
         with warnings.catch_warnings():
             warnings.simplefilter('ignore')
-            cur.execute('CREATE TABLE IF NOT EXISTS matches ' +
-                        '(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, ' +
-                        'team1 VARCHAR(100) NOT NULL, ' +
-                        'team2 VARCHAR(100) NOT NULL, ' +
-                        'score1 SMALLINT NOT NULL, ' +
-                        'score2 SMALLINT NOT NULL, ' +
-                        'tournament VARCHAR(256), ' +
-                        '`date` DATE NOT NULL)')
+            cur.execute(self.create_table_query)
+            self.conn.commit()
+
         cur.execute("SELECT * FROM matches")
         matches = []
         for m in cur.fetchall():
             matches.append(Match(m["team1"],
                                  m["team2"],
-                                 m["score1"],
-                                 m["score2"],
+                                 str(m["score1"]),
+                                 str(m["score2"]),
                                  m["tournament"],
-                                 m["date"].day,
-                                 m["date"].month,
-                                 m["date"].year))
+                                 str(m["date"].day),
+                                 str(m["date"].month),
+                                 str(m["date"].year)))
         return tuple(matches)
 
     def create(self, team1, team2, score1, score2, tournament, dd, mm, yy):
@@ -137,14 +160,17 @@ class MatchMysqlDb(MatchList):
             raise DateError("Date can't be empty, less than 01-01-1000 " +
                             "or greater than 31-12-9999")
 
-        cur = self.conn.cursor(MySQLdb.cursors.DictCursor)
-        cur.execute('INSERT INTO matches ' +
-                    '(team1, team2, score1, score2, tournament, date) ' +
-                    'VALUES' +
-                    '("{}", "{}", {}, {}, "{}", "{}-{}-{}")'.format(
-                        team1, team2, score1, score2, tournament, yy, mm, dd
-                    ))
-        return Match(team1, team2, score1, score2, tournament, dd, mm, yy)
+        cur = self._getcur()
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            cur.execute(self.create_table_query)
+
+        data = (team1, team2, score1, score2,
+                tournament, datetime.date(yy, mm, dd))
+        cur.execute(self.insert_query, data)
+        self.conn.commit()
+        return Match(team1, team2, str(score1), str(score2),
+                     tournament, str(dd), str(mm), str(yy))
 
     def edit(self, idx, team1, team2, score1, score2, tournament, dd, mm, yy):
         try:
@@ -168,35 +194,33 @@ class MatchMysqlDb(MatchList):
         except IndexError:
             return None
 
-        cur = self.conn.cursor(MySQLdb.cursors.DictCursor)
-        cur.execute('UPDATE matches SET ' +
-                    'team1="{}", team2="{}", score1={}, score2={}, '.format(
-                        team1, team2, score1, score2) +
-                    'tournament="{}", date="{}-{}-{}" '.format(
-                        tournament, yy, mm, dd) +
-                    'WHERE team1 = "{}" AND team2 = "{}" '.format(
-                        old_match.team1, old_match.team2) +
-                    'AND score1 = {} AND score2 = {} '.format(
-                        old_match.score1, old_match.score2) +
-                    'AND tournament = "{}" AND date = "{}-{}-{}"'.format(
-                        old_match.tournament, old_match.yy,
-                        old_match.mm, old_match.dd))
-        return Match(team1, team2, score1, score2, tournament, dd, mm, yy)
+        cur = self._getcur()
+
+        data = (team1, team2, score1, score2, tournament,
+                datetime.date(yy, mm, dd), old_match.team1, old_match.team2,
+                old_match.score1, old_match.score2, old_match.tournament,
+                datetime.date(int(old_match.yy),
+                              int(old_match.mm),
+                              int(old_match.dd)))
+        cur.execute(self.update_query, data)
+        self.conn.commit()
+        return Match(team1, team2, str(score1), str(score2),
+                     tournament, str(dd), str(mm), str(yy))
 
     def delete(self, idx):
         try:
             match = self.show()[idx]
         except IndexError:
             return None
-        cur = self.conn.cursor(MySQLdb.cursors.DictCursor)
-        cur.execute('DELETE FROM matches WHERE ' +
-                    'team1 = "{}" AND team2 = "{}" '.format(
-                        match.team1, match.team2) +
-                    'AND score1 = {} AND score2 = {} '.format(
-                        match.score1, match.score2) +
-                    'AND tournament = "{}" AND date = "{}-{}-{}"'.format(
-                        match.tournament, match.yy,
-                        match.mm, match.dd))
+
+        cur = self._getcur()
+
+        data = (match.team1, match.team2, match.score1, match.score2,
+                match.tournament, datetime.date(int(match.yy),
+                                                int(match.mm),
+                                                int(match.dd)))
+        cur.execute(self.delete_query, data)
+        self.conn.commit()
         return match
 
 
